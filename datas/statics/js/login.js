05d3da1c73da8ccf89b553263f94064d089e4f3c
moyan <1561515308@qq.com>
2023-02-19 16:33:10 +0800

    Add files via upload

---
+++ datas/statics/js/login.js
@@ -0,0 +1,18 @@
+function logins() {
+	var text = document.getElementById("password").value;
+	var url = "/token?pass=" + text
+	$.get(url, function(datas) {
+		var url = "token/verify?token=" + datas
+		$.get(url, function(data) {
+			console.log(data)
+			if (data == "Yes") {
+				setCookie("token", datas, 90)
+				window.location.href = '/'
+			} else if (data == "No") {
+				alert("密码错误")
+				document.getElementById("password").value = ""
+			}
+		})
+
+	})
+}
